3510a23a68571b4ab731e48c7b31505f48226886
EC2 Default User <ec2-user@ip-172-31-15-94.ap-northeast-1.compute.internal>
2022-12-29 13:59:32 +0000

    [middle]投稿詳細画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 class ApplicationController < ActionController::Base
+# devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行
+before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+  protected
+  # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
 end
