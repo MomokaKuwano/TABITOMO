5eb963c1bdcacdf463dd2a986fb84c0a55fb2c78
EC2 Default User <ec2-user@ip-172-31-15-94.ap-northeast-1.compute.internal>
2022-12-30 11:03:01 +0000

    [middle]マイページ

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,15 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
+    @posts = @user.posts.published
   end
 
   def edit
+    @user = current_user
+  end
+    
+  def update
+    user.update(user_params)
+    redirect_to mypage_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  has_one_attached :profile_image
+
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/user_no_image.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
