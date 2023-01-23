3337d0f44beb0181990bbd5a2d6a76d411b9efd4
EC2 Default User <s2.spnj2m@gmail.com>
2023-01-23 14:15:53 +0000

    [finish]pack/index

--- app/controllers/public/packs_controller.rb
+++ app/controllers/public/packs_controller.rb
@@ -1,32 +1,29 @@
 class Public::PacksController < ApplicationController
 
   def index
-    # @item = Item.new
-    # @items = current_user.items.all
-    # @item_id = @items.pluck(:id)
-    # @packs = current_user.packs.all
-     @pack = Pack.new
+
+    @packs = current_user.packs.all
+    @pack = Pack.new
   end
 
   def create
-    # @packs = current_user.packs.all
-    # @items = current_user.items.all
-    # @item_ids = @items.pluck(:id)
-    # @pack = Pack.new(pack_params)
-    # if @pack.save
-    #   @item_ids.each do |item_id_data|
-        # @packing_list = @pack.packing_lists.new
-    #     @packing_list.pack_id = @pack.id
-    #     @packing_list.item_id = item_id_data
-    #     # @packing_list = @pack.packing_lists.new(item_id: item_id_data)
-    #     @packing_list.save
-    #   end
-    #   redirect_to pack_path(@pack)
-    # else
-    #   # flash
-    #   render :index
-    # end
-    redirect_to pack_path(Pack.last)
+    # 空のitem作成
+    @items = []
+    # hashのeach文でitemのnameを保存する
+    params[:pack][:packing_lists_attributes].each do |k, v|
+      @items << Item.find_or_create_by(name: v[:item_id], user_id: current_user.id)
+    end
+    @pack = Pack.new(pack_params)
+    @pack.user_id = current_user.id
+    if @pack.save
+      # packingリストにそれぞれのidを保存
+      @items.each do |item|
+        PackingList.create(pack_id: @pack.id, item_id: item.id)
+      end
+      redirect_to pack_path(Pack.last)
+    else
+      render :index
+    end
   end
 
   def show
@@ -55,9 +52,9 @@ class Public::PacksController < ApplicationController
   #   params.require(:pack).permit(:pack_title).merge(user_id: current_user.id)
   # end
   def pack_params
-    params.require(:pack).permit(
-      packing_lists_attributes: [:id, :item_id, :name, :_destroy]
-    )
+    params.require(:pack).permit(:pack_title)
+    #   packing_lists_attributes: [:id, :item_id, :name, :_destroy]
+    # )
   end
 
 
