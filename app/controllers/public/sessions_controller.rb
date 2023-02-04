31494a39e36d04d15a8832d3fee586a30e485093
EC2 Default User <s2.spnj2m@gmail.com>
2023-02-04 09:28:49 +0000

    [finish]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,8 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :user_state, only: [:create]
+
   def guest_sign_in
     user = User.guest
     sign_in user
@@ -22,7 +24,19 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 退会しているかを判断するメソッド
+  def user_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @user = User.find_by(email: params[:user][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@user
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @user.valid_password?(params[:user][:password]) && @user.is_deleted == true
+    ## 【処理内容3】
+    redirect_to new_user_registration_path
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
